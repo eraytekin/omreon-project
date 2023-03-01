04ee5959456848794ff0e360be3da4402e75fb9c
Eray Tekin <eray.tekin2934@outlook.com>
2023-03-02 02:22:27 +0300

    navbar ikinci save icon ve responsive eklendi

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react'
+
 import "./navbar.css";
+import {FaAlignJustify, FaTimes} from 'react-icons/fa';
 
 function Navbar() {
   return (
     <div>
         <div className="navbar-container">
           <div className="navbar-left-side">
-            <div className="hamburger-button"><button>H. But.</button></div>
+            <div className="hamburger-button"><FaAlignJustify style={{color: 'black', fontSize: '25px' }}/></div>
           </div>
           <div className="navbar-right-side">
             <ul className="menu">
@@ -15,8 +17,8 @@ function Navbar() {
               <li><a href="#">Teknolojiler</a></li>
               <li><a href="#">İnsan Kaynakları</a></li>
               <li><a href="#">Kurumsal</a></li>
-              <li><a href="#">İletişim</a></li>
             </ul>
+            <button className="contact-button">İletişim</button>
           </div>
         </div>
     </div>
