b98bc1f2facc64d40762aeef8aefe2477165736c
Eray Tekin <eray.tekin2934@outlook.com>
2023-03-02 15:54:30 +0300

    genel görünüm oluştu navbar responsive slider area mobilde opacity kaldı

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -8,6 +8,35 @@ function slider() {
         
         <div className="slider-container">
             <Navbar/>
+            <div className="slider-inner">
+
+              <div className="slider-inner-left-side">
+
+                <div className="left-side-inner">
+
+                  <div className="slider-header">Bilgi Teknolojilerinde</div>
+
+                  <div className="slider-header">23 YILLIK TECRÜBE</div>
+
+                  <p className="slider-content">
+                  Müşterilerimizin yüksek kalite seviyelerini koruyabilmeleri için farklı sektörlerde tecrübe kazanmış 
+                  uzman kadrolarımızla Proje Yönetimi, İş Analizi ve Test Yönetimi hizmetleri sunmaktayız.
+                  </p>
+
+                  <div className="subscribe-newsletter">
+                    <input type="text" placeholder='Mail bültenimize kayıt ol'/>
+                    <button className='sign-up-newsletter'>Kayıt Ol</button>
+                  </div>
+                  
+                </div>
+
+              </div>
+
+              <div className="slider-inner-right-side">
+                {/* right side */}
+              </div>
+
+            </div>
         </div>
     </div>
   )
